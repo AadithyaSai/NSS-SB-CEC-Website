9e1d6e1cb0316c584d73c9e1b6b08cb3a9f9efa9
AadithyaSai <aadithyas0610@gmail.com>
2024-05-06 15:25:44 +0530

    Revert "Fixed events"
    This reverts commit 7fb93ca2aa4591379c9bc9f7a42e5a15dc2eaa81.

--- js/login.js
+++ js/login.js
@@ -1,63 +1,16 @@
-import { getDoc, doc } from "firebase/firestore";
-import { auth, db } from "./firebaseconfig";
-import {
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  sendPasswordResetEmail,
-} from "firebase/auth";
+import { auth } from "./firebaseconfig";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    getDoc(doc(db, "users", user.uid)).then((doc) => {
-      console.log(doc.data());
-      if (doc.data().isAdmin) {
-        window.location.href = "adminprofile";
-      } else {
-        window.location.href = "profile";
-      }
-    });
+    window.location.href = "profile";
   } else {
-    const msgBox = document.getElementById("msgBox");
-    const msgP = document.querySelector("#msgBox p");
-    const loginForm = document.getElementById("loginForm");
-    const forgotForm = document.getElementById("forgotForm");
+    const form = document.getElementById("loginForm");
     const usernameField = document.getElementById("usernameField");
-    const forgotEmailField = document.getElementById("forgotUsernameField");
     const passwordField = document.getElementById("passwordField");
-    const loginContent = document.getElementById("loginContent");
-    const forgotContent = document.getElementById("forgotContent");
-    const loginTxt = document.getElementById("loginTxt");
-    const loginSpinner = document.getElementById("loginSpinner");
 
-    document.getElementById("forgotLink").addEventListener("click", () => {
-      loginContent.classList.toggle("disabled");
-      forgotContent.classList.toggle("disabled");
-    });
-
-    document.getElementById("backLink").addEventListener("click", () => {
-      loginContent.classList.toggle("disabled");
-      forgotContent.classList.toggle("disabled");
-    });
-
-    forgotForm.addEventListener("submit", (event) => {
+    form.addEventListener("submit", (event) => {
       event.preventDefault();
-
-      const email = forgotEmailField.value;
-
-      sendPasswordResetEmail(auth, email)
-        .then(() => {
-          showToast("Password reset email sent!");
-        })
-        .catch((err) => {
-          showToast("Error sending password reset email: " + err);
-        });
-    });
-
-    loginForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      loginTxt.classList.toggle("opacity-0");
-      loginSpinner.classList.toggle("opacity-0");
 
       const email = usernameField.value;
       const password = passwordField.value;
@@ -66,21 +19,11 @@ onAuthStateChanged(auth, (user) => {
         .then((userCredential) => {
           const user = userCredential.user;
           console.log(`Signed up as: ${user.email}`);
+          window.location.href = "profile.html";
         })
         .catch((err) => {
-          loginTxt.classList.toggle("opacity-0");
-          loginSpinner.classList.toggle("opacity-0");
-          usernameField.classList.add("is-invalid");
-          passwordField.classList.add("is-invalid");
-          showToast("Error signing in: " + err);
+          console.log(`Error: ${err.code} ${err.message}`);
         });
     });
   }
 });
-
-function showToast(message) {
-  const toastLiveExample = document.getElementById("liveToast");
-  document.getElementById("toastBody").innerText = message;
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
-  toastBootstrap.show();
-}
--- js/profile.js
+++ js/profile.js
@@ -13,18 +13,19 @@ onAuthStateChanged(auth, (user) => {
   if (user) {
     uid = user.uid;
 
-    const signOutLink = document.getElementById("signOutLink");
+    const contentDiv = document.getElementById("content");
+    const p = document.createElement("p");
+    p.innerHTML = `hey ${user.email} <br />`;
+    const signOutLink = document.createElement("a");
+    signOutLink.text = "Sign Out";
+    signOutLink.href = "/login.html";
     signOutLink.onclick = () => {
       signOut(auth)
         .then(() => console.log("User has been signed out successfully"))
         .catch((err) => console.log(err));
     };
-
-    const contentDiv = document.getElementById("content");
-    const p = document.createElement("p");
-    p.id = "greeting";
-    // contentDiv.prepend(signOutLink);
-    // contentDiv.prepend(p);
+    p.appendChild(signOutLink);
+    contentDiv.prepend(p);
 
     const userData = doc(collection(db, "users"), uid);
 
@@ -41,8 +42,8 @@ onAuthStateChanged(auth, (user) => {
       const bloodProofRef = ref(storage, `${uid}/blood/proof`);
       uploadBytes(bloodProofRef, bloodProofFile).then((snapshot) => {
         console.log("Uploaded proof!");
-        bloodForm.classList.toggle("d-none");
-        bloodDone.classList.toggle("d-none");
+        bloodForm.classList.add("hide");
+        bloodDone.classList.remove("hide");
         updateDoc(userData, {
           bloodDone: true,
         });
@@ -58,30 +59,26 @@ onAuthStateChanged(auth, (user) => {
         updateDoc(userData, {
           thanalDone: true,
         });
-        thanalForm.classList.toggle("d-none");
-        thanalDone.classList.toggle("d-none");
+        thanalForm.classList.add("hide");
+        thanalDone.classList.remove("hide");
         console.log("Uploaded proof!");
       });
     });
 
     getDoc(userData)
       .then((snapshot) => {
-        document.getElementById("content").classList.toggle("d-none");
-        document.getElementById("wait").classList.toggle("d-none");
+        document.getElementById("content").classList.remove("hide");
+        document.getElementById("wait").classList.add("hide");
         const hours = snapshot.data().hours;
-        const name = snapshot.data().name;
-        const unit = snapshot.data().unit;
         document.getElementById("hourCount").textContent = hours;
-        document.getElementById("name").textContent = name;
-        document.getElementById("unit").textContent = unit;
 
         if (snapshot.data().bloodDone) {
-          bloodForm.classList.toggle("d-none");
-          bloodDone.classList.toggle("d-none");
+          bloodForm.classList.add("hide");
+          bloodDone.classList.remove("hide");
         }
         if (snapshot.data().thanalDone) {
-          thanalForm.classList.toggle("d-none");
-          thanalDone.classList.toggle("d-none");
+          thanalForm.classList.add("hide");
+          thanalDone.classList.remove("hide");
         }
       })
       .catch((e) => console.log(`Error: ${e}`));
